d603e02bc9874c53dc42b5dc2f291f7f83b3c989
Geradine <geradinenyakundi53@gmail.com>
2023-04-14 23:30:38 +0300

    firebase db

--- login.js
+++ login.js
@@ -1,3 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+import{
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFIqyWsBThBklFT8svjUUerXJgX7vJEWA",
+    authDomain: "chama-e7e54.firebaseapp.com",
+    projectId: "chama-e7e54",
+    storageBucket: "chama-e7e54.appspot.com",
+    messagingSenderId: "911475630973",
+    appId: "1:911475630973:web:3eb17990d13a56b35a6214",
+    measurementId: "G-HRXP9QSKZ8"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+
 //login field
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
@@ -15,8 +40,88 @@ const createacct = document.getElementById("create-acct");
 
 const signupButton = document.getElementById("sign-up");
 const returnBtn = document.getElementById("return-btn");
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignupEmail,
+  confirmSignUpPassword;
 
-signupButton.addEventListener("click", function(){
+createacctbtn.addEventListener("click", function () {
+  var isVerified = true;
+
+  signupEmail = signupEmailIn.value;
+  confirmSignupEmail = confirmSignupEmailIn.value;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  if (
+    signupEmail == null ||
+    confirmSignupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+
+  if (isVerified) {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+        // Signed in
+        // const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+        window.location = "./createTask.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+        window.alert(errorMessage);
+      });
+  }
+});
+
+submitButton.addEventListener("click", function () {
+  email = emailInput.value;
+  // console.log(email);
+  password = passwordInput.value;
+  // console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      window.alert("Success! Welcome back!");
+      window.location = "./createTask.html";
+
+      // ...
+    })
+    .catch((error) => {
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      window.alert("Error occurred. Try again.");
+    });
+});
+
+signupButton.addEventListener("click", () => {
   main.style.display = "none";
   createacct.style.display = "block";
-})
+});
+
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
+});
